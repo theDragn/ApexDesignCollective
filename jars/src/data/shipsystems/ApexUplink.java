3caf46966da9f170aa916242b5ad08dda32ae5db
theDragn <61987648+theDragn@users.noreply.github.com>
2022-10-06 04:06:32 -0400

    sonora system wip

--- jars/src/data/shipsystems/ApexUplink.java
+++ jars/src/data/shipsystems/ApexUplink.java
@@ -5,9 +5,12 @@ import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
+import org.lazywizard.lazylib.MathUtils;
 import org.lazywizard.lazylib.combat.CombatUtils;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class ApexUplink extends BaseShipSystemScript
 {
@@ -15,8 +18,10 @@ public class ApexUplink extends BaseShipSystemScript
     public static final float SHIELD_EFFICIENCY_MULT = 0.8f;
     public static final float ARMOR_EFFECTIVE_MULT = 1.2f;
     public static final String BUFF_ID = "apex_uplink_sys";
+    public static final Color JITTER_COLOR = new Color(0, 190, 0);
 
     private ArrayList<ApexUplinkBuff> buffs = new ArrayList<>();
+    protected HashSet<ShipAPI> targets = new HashSet<>();
     
 
     @Override
@@ -31,8 +36,8 @@ public class ApexUplink extends BaseShipSystemScript
             if (target.isAlive() && target.getOwner() == ship.getOwner())
             {
                 ApexUplinkBuff buff = new ApexUplinkBuff(ship, target);
+                targets.add(target);
                 buffs.add(buff);
-                Global.getCombatEngine().addPlugin(buff);
             }
 
         }
@@ -50,37 +55,47 @@ public class ApexUplink extends BaseShipSystemScript
     {
         if (stats.getEntity() == null)
             return;
-        super.unapply(stats, id);
+        float amount = Global.getCombatEngine().getElapsedInLastFrame();
+        for (ApexUplinkBuff buff : buffs)
+        {
+            buff.update(amount);
+        }
     }
 
-    private class ApexUplinkBuff extends BaseEveryFrameCombatPlugin
+    private class ApexUplinkBuff
     {
         private float lifetime;
         private ShipAPI target;
+        private ShipAPI source;
 
-        public ApexUplinkBuff(ShipAPI target)
+        public ApexUplinkBuff(ShipAPI source, ShipAPI target)
         {
             this.target = target;
+            this.source = source;
             this.lifetime = 0.2f;
         }
 
         // advance()
-        public boolean update(float amount)
+        public void update(float amount)
         {
             // should probably do graphical stuff in here too
-            lifetime -= amount;
             MutableShipStatsAPI stats = target.getMutableStats();
-            if (lifetime > 0)
+            if (!target.isAlive() || !source.isAlive() || MathUtils.getDistanceSquared(target.getLocation(), source.getLocation()) > RANGE * RANGE)
             {
                 stats.getEffectiveArmorBonus().modifyMult(BUFF_ID, ARMOR_EFFECTIVE_MULT);
                 stats.getShieldDamageTakenMult().modifyMult(BUFF_ID, SHIELD_EFFICIENCY_MULT);
-                return true;
             } else
             {
                 stats.getEffectiveArmorBonus().unmodify(BUFF_ID);
                 stats.getShieldDamageTakenMult().unmodify(BUFF_ID);
-                return false;
+                lifetime -= amount;
+                if (lifetime == 0)
+                {
+                    targets.remove(target);
+                    buffs.remove(this);
+                }
             }
+            ((ShipAPI)stats.getEntity()).setJitterUnder(BUFF_ID, JITTER_COLOR, 3f * lifetime * 5f, 3, 0f, 25f * lifetime);
         }
     }
 }
