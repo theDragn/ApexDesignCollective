1c0f8b9d4f17b5c72c5a431035dd381974a92c38
theDragn <61987648+theDragn@users.noreply.github.com>
2022-12-01 12:20:34 -0500

    more goanna/quasar/iguana

---
+++ jars/src/data/shipsystems/ApexPursuitJets.java
@@ -0,0 +1,58 @@
+package data.shipsystems;
+
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
+import com.fs.starfarer.api.plugins.ShipSystemStatsScript;
+import com.fs.starfarer.api.util.Misc;
+import org.lazywizard.lazylib.MathUtils;
+import org.lazywizard.lazylib.VectorUtils;
+
+import static data.ApexUtils.text;
+
+public class ApexPursuitJets extends BaseShipSystemScript
+{
+    public static final float MAX_BOOST_ARC = 45f;  // degrees to either side where boost = 100%
+    public static final float BOOST_MIN = 90f;      // degrees to either side where boost = 0%
+    public static final float MAX_SPEED_BONUS = 100f;
+    public static final float ACCEL_MULT = 2f;
+    public static final float TURN_MULT = 1.5f;
+
+    public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel)
+    {
+        if (!(stats.getEntity() instanceof ShipAPI))
+            return;
+        ShipAPI ship = (ShipAPI) stats.getEntity();
+
+        // angle of the ship's current velocity, from forwards
+        float velAngle = Math.abs(MathUtils.getShortestRotation(ship.getFacing(), VectorUtils.getAngle(Misc.ZERO, ship.getVelocity())));
+        float boostAmount = 0f;
+        if (velAngle < MAX_BOOST_ARC)
+            boostAmount = 1;
+        else if (velAngle < BOOST_MIN)
+            boostAmount = 1f - (velAngle - MAX_BOOST_ARC) / (BOOST_MIN - MAX_BOOST_ARC);
+
+        stats.getMaxSpeed().modifyFlat(id, boostAmount * MAX_SPEED_BONUS * effectLevel);
+        stats.getAcceleration().modifyMult(id, ACCEL_MULT);
+        stats.getTurnAcceleration().modifyMult(id, TURN_MULT);
+        stats.getMaxTurnRate().modifyMult(id, TURN_MULT);
+    }
+
+    public void unapply(MutableShipStatsAPI stats, String id)
+    {
+        stats.getMaxSpeed().unmodify(id);
+        stats.getAcceleration().unmodify(id);
+        stats.getMaxTurnRate().unmodify(id);
+        stats.getTurnAcceleration().unmodify(id);
+    }
+
+    public StatusData getStatusData(int index, State state, float effectLevel)
+    {
+        if (index == 0)
+        {
+            return new StatusData(text("moveburst"), false);
+        }
+        return null;
+    }
+}
+
