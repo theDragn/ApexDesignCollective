92f3c87dd52f3aee59be3b32c109e0e8aae4149f
theDragn <61987648+theDragn@users.noreply.github.com>
2023-04-08 03:31:18 -0400

    weapon tweaks and release script

---
+++ make_release.py
@@ -0,0 +1,15 @@
+import shutil, pathlib
+
+curdir = str(pathlib.Path(__file__).parent.resolve())
+modname = "Apex Design Collective"
+
+src = curdir
+temp = curdir + "\\temp\\" + modname
+toIgnore = shutil.ignore_patterns('*.gitignore', '.idea', '.run', 'out', 'src', '*.iml','*.py','*.md','.git','temp','changes.txt','apex_lineup.png')
+print("\nDid you update the version number in BOTH the mod_info and the .version?\n")
+print("Copying to temp directory...")
+shutil.copytree(src, temp, ignore = toIgnore)
+print("Creating .zip...")
+shutil.make_archive(modname + '_v###', 'zip', temp)
+print("Deleting temp directory...")
+shutil.rmtree(curdir + "\\temp")
