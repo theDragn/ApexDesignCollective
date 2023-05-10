480c1f8f70c98c0e34f39ce92a79d52e069b18f5
theDragn <61987648+theDragn@users.noreply.github.com>
2023-05-10 01:49:52 -0400

    release script fix

--- make_release.py
+++ make_release.py
@@ -5,11 +5,11 @@ modname = "Apex Design Collective"
 
 src = curdir
 temp = curdir + "\\temp\\" + modname
-toIgnore = shutil.ignore_patterns('*.gitignore', '.idea', '.run', 'out', 'src', '*.iml','*.py','*.md','.git','temp','changes.txt','apex_lineup.png')
+toIgnore = shutil.ignore_patterns('*.gitignore', '.idea', '.run', 'out', 'src', '*.iml','*.py','*.md','.git','temp','changes.txt','apex_lineup.png','.gitattributes')
 print("\nDid you update the version number in BOTH the mod_info and the .version?\n")
 print("Copying to temp directory...")
 shutil.copytree(src, temp, ignore = toIgnore)
 print("Creating .zip...")
-shutil.make_archive(modname + '_v###', 'zip', temp)
+shutil.make_archive(modname + '_v###', 'zip', curdir + "\\temp")
 print("Deleting temp directory...")
 shutil.rmtree(curdir + "\\temp")
