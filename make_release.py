e2044899796788c367beb9d848b4312aee2f0187
theDragn <61987648+theDragn@users.noreply.github.com>
2024-06-05 15:04:16 -0400

    1.2.9 forum release

--- make_release.py
+++ make_release.py
@@ -5,7 +5,7 @@ modname = "Apex Design Collective"
 
 src = curdir
 temp = curdir + "\\temp\\" + modname
-toIgnore = shutil.ignore_patterns('*.gitignore', '.idea', '.run', 'out', 'src', '*.iml','*.py','*.md','.git','temp','changes.txt','apex_lineup.png','.gitattributes')
+toIgnore = shutil.ignore_patterns('*.gitignore', '.idea', '.run', 'out', 'src', '*.iml','*.py','*.md','.git','temp','changes.txt','apex_lineup.png','.gitattributes',"apex_lineup_relic.png")
 print("\nDid you update the version number in BOTH the mod_info and the .version?\n")
 print("Copying to temp directory...")
 shutil.copytree(src, temp, ignore = toIgnore)
